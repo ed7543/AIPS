afe53ece4117ff7aa3a25ce9963a3a87624257d2
ed7543 <100047224+ed7543@users.noreply.github.com>
2023-12-05 19:25:01 +0100

    Update Brisenje.java
    final algorithm za brisenje

--- Brisenje.java
+++ Brisenje.java
@@ -2,29 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void add(int arr[], int n, int k) {
-        int newarr[] = new int[n];
-        int i;
-
-        for (i = 1; i < k + 1; i++) {
-            newarr[i] = arr[i - 1];
-            
-        }
-      
-        
-        for (i = k + 1; i < n; i++) {
-            
-            newarr[i] = arr[i];
-        }
-
-        
-        for(i = 0; i < newarr.length - 1; i++) {
-            newarr[i] = newarr[i + 1];
-        }
-        
-        newarr = Arrays.copyOf(newarr, newarr.length - 1);
-        
-
-        System.out.println(Arrays.toString(newarr));
+       for(int i = k; i < n - 1; i++) {
+           arr[i] = arr[i + 1];
+       }
+       
+       arr = Arrays.copyOf(arr, n - 1);
+       System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
