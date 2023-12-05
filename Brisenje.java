284af61650f25fb4f7462c20a2054bd4a23955e9
ed7543 <100047224+ed7543@users.noreply.github.com>
2023-12-05 19:06:23 +0100

    Create Brisenje.java

---
+++ Brisenje.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void add(int arr[], int n, int k) {
+        int newarr[] = new int[n];
+        int i;
+
+        for (i = 1; i < k + 1; i++) {
+            newarr[i] = arr[i - 1];
+            
+        }
+      
+        
+        for (i = k + 1; i < n; i++) {
+            
+            newarr[i] = arr[i];
+        }
+
+        for(i = 1; i < n; i++) {
+            System.out.print(newarr[i] + " ");
+        }
+
+        //System.out.println(Arrays.toString(newarr));
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int k = 5;
+        System.out.println(Arrays.toString(arr));
+
+        add(arr, n, k);
+    }
+}
